9d46b2e974048e23976f7e3e2b990539bd68796e
kmpraveen <kmpraveen@juniper.net>
2024-03-12 21:58:56 +0530

    removing api keys

--- generative-ai/simple-rag-with-pdfdata/chatpdf.py
+++ generative-ai/simple-rag-with-pdfdata/chatpdf.py
@@ -11,7 +11,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-GOOGLE_API_KEY = "AIzaSyAiHHKtBRKDrAjr0o6mt2gK12FUICpxz7k"
+GOOGLE_API_KEY = "<PASTE YOUR GOOGLE_API_KEY HERE>"
 load_dotenv()
 os.getenv(GOOGLE_API_KEY)
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
